02d68ab75d324fd2829fe558a8b0a8961ebbd54d
Marcos <mvmendesferreira@id.uff.br>
2025-08-15 01:10:05 -0300

    adicionando input de pesquisa

--- src/pages/Genero/Genero.tsx
+++ src/pages/Genero/Genero.tsx
@@ -1,4 +1,5 @@
 import { Link, useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import type { Livro } from "../../Types/Livro";
 import arrow from "../../assets/arrow.png";
 import LivrosDoGenero from "../../components/LivrosDoGenero/LivrosDoGenero";
@@ -11,19 +12,39 @@ type GeneroPageData = {
 
 export default function Genero() {
     const { genero, livros } = useLoaderData() as GeneroPageData;
+    const [termoPesquisa, setTermoPesquisa] = useState("");
+
+    const livrosFiltrados = livros.filter(livro =>
+        livro.titulo.toLowerCase().includes(termoPesquisa.toLowerCase())
+    );
 
     return (
         <div className={styles.container}>
+
+            <input
+                type="text"
+                placeholder="Pesquisar por título"
+                value={termoPesquisa}
+                onChange={(e) => setTermoPesquisa(e.target.value)}
+                className={styles.input}
+            />
+
             <div className={styles.header}>
                 <Link to="/generos" style={{height: "20px"}}><img src={arrow} alt="Voltar" className={styles.arrow} /></Link>
                 <h1 className={styles.titulo}>{genero}</h1>
             </div>
 
             <div className={styles.gridLivros}>
-                {livros.map((livro: Livro) => (
+                {livrosFiltrados.map((livro: Livro) => (
                     <LivrosDoGenero key={livro.id} livrosDoGenero={livro} />
                 ))}
             </div>
+
+            {livrosFiltrados.length === 0 && termoPesquisa && (
+                <div className={styles.naoEncontrado}>
+                    <p>Nenhum livro encontrado com o termo "{termoPesquisa}"</p>
+                </div>
+            )}
         </div>
     )
 }
