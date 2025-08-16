506050fc90250b8f40a5a528804ab7f2309f99aa
Marcos <mvmendesferreira@id.uff.br>
2025-08-16 11:16:32 -0300

    fix: mudando os width para vw e vh

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
+import Login from "./pages/Login/Login"
 
 function App() {
 
   return (
     <>
       <RouterProvider router={router} />
+      {/* <Login /> */}
+
+      {/* COLOCAR REQUISIÇÃO HTTP E HOVERS  */}
     </>
   )
 }
