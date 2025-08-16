fa243672c91de3e07e54ab121d3ac738823af941
Marcos <mvmendesferreira@id.uff.br>
2025-08-16 10:35:25 -0300

    add: criando form login

---
+++ src/components/FormLogin/FormLogin.tsx
@@ -0,0 +1,48 @@
+import { zodResolver } from "@hookform/resolvers/zod/src/index.js";
+import { useForm } from "react-hook-form"
+import type { User } from "../../Types/Users";
+import userSchema from "../../Types/Users";
+import styles from './styles.module.css';
+
+export default function FormLogin() {
+    const {register, handleSubmit, reset, formState: {errors, isSubmitting}, setError} = useForm<User>({
+        resolver: zodResolver(userSchema)
+    });
+
+    async function createUser(data : User){
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log(data);
+        reset();
+    }
+
+    return (
+        <form onSubmit={handleSubmit(createUser)} className={styles.form}>
+            <div className={styles.inputs}>
+                <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
+                    <p>E-mail</p>
+                    <input 
+                        type="text" 
+                        placeholder="Email" 
+                        {...register("email")}
+                    />
+                    {errors.email && <span>{errors.email.message}</span>}
+                </div>
+
+                <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
+                    <p>Senha</p>
+                    <input 
+                        type="password" 
+                        placeholder="Senha" 
+                        {...register("senha")}
+                    />
+                    {errors.senha && <span>{errors.senha.message}</span>}
+                </div>
+            </div>
+
+            <div className={styles.buttons}>
+                <button className={styles.entrar} disabled={isSubmitting}>{isSubmitting ? "Carregando..." : "Entrar"}</button>
+                <button className={styles.cadastrar}>Cadastre-se</button>
+            </div>
+        </form>
+    )
+}
