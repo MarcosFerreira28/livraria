8b1256c222fdb8a927a4c0ef92df9ffe58369dc9
Marcos <mvmendesferreira@id.uff.br>
2025-08-16 21:26:12 -0300

    add: adicionando css para o carrinho

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,23 +1,24 @@
+import styles from "./styles.module.css";
 import useCartStore from "../../stores/CartStore";
 
 export default function Cart(){
     
     const cart = useCartStore((state) => state.cart);
     const removeFromCart = useCartStore((state) => state.removeFromCart);
-
     const total = cart.reduce((acc, livro) => acc + livro.preco, 0);
-    return(
-        <>
-            <h1>Carrinho de Compras</h1>
-            <ul>
+
+    return (
+        <div className={styles.cartContainer}>
+            <h1 className={styles.cartTitle}>Carrinho de Compras</h1>
+            <ul className={styles.cartList}>
                 {cart.map((livro, indice) => (
-                    <li key={indice}>
+                    <li key={indice} className={styles.cartItem}>
                         <span>{livro.titulo} - R$ {livro.preco.toFixed(2)}</span>
-                        <button onClick={() => removeFromCart(livro.id)}>Remover</button>
+                        <button className={styles.removeButton} onClick={() => removeFromCart(livro.id)}>Remover</button>
                     </li>
                 ))}
             </ul>
-            <h2>Total: R$ {total.toFixed(2)}</h2>
-        </>
-    )
+            <h2 className={styles.cartTotal}>Total: R$ {total.toFixed(2)}</h2>
+        </div>
+    );
 }
